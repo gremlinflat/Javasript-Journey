84049fec6cb0d38d433cdb6415d78b1b64db1fd7
Gremlinflat <fahri.119140205@student.itera.ac.id>
2021-01-11 04:30:05 +0700

    YEAY FIRST DAT COMMIT 1 FUNDAMENTAL

---
+++ 01-Fundamental/script.js
@@ -0,0 +1,54 @@
+/*let markMass = 78, johnMass = 92, markHeight = 1.69, johnHeight = 1.95;
+
+bmiMark = markMass / (markHeight ** 2);
+bmiJohn = johnMass / (johnHeight ** 2);
+markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI);*/
+/* myname = `jonas`
+birth = 1985
+now = "2020"
+job = `teacher`
+
+jonas = `my name is ${myname}, im ${now - birth} year old, im a ${job}`
+console.log(jonas)
+
+console.log(Number(now) + 11) */
+
+/*
+yourname = prompt("Enter Your name ");
+yourbirth = prompt("Enter Your birth year ");
+yourjob = prompt("Enter Your job ");
+
+biodate = `your name is ${yourname}, you're ${2020 - yourbirth} year old, and you're a ${yourjob}`;
+
+console.log(biodate); */
+
+// === is for strict comparison (same data type)
+// == is for comparing & flexible data type
+/* if (yourname === "joji") {
+    console.log("IMMA FUCK UP MY LIFE")
+}
+if ((2020 - yourbirth) == 18) {
+    console.log("YOU YOURE 18 now")
+} */
+
+//switch
+/* day = "sunday";
+switch (day) {
+    case "monday":
+        console.log("its monday")
+        break;
+    case "sunday":
+        console.log("its ter")
+        break;
+    case "wed":
+        console.log("its wed")
+        break;
+
+    default:
+        break;
+} */
+
+
+age = 16;
+age >= 18 ? console.log("YEAY LETS DRINK WINE") : console.log("just drink feeng water")
